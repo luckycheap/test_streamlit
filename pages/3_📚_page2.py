372034da91acbbd95991dc3cb53b0a6616a5ca50
luckycheap <130981667+luckycheap@users.noreply.github.com>
2024-01-10 10:36:39 +0100

    ajout des pages

---
+++ pages/3_📚_page2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_icon= "📚",
+                   page_title = "page 2 mais 3")
+
+
+st.title("page3")
